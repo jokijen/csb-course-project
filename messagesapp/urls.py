024b840dd22b69e0f0d6560605559d729a70e5f6
Jenni Jokinen <153839491+jokijen@users.noreply.github.com>
2024-08-16 20:08:48 +0300

    Changes related to adding register, login, logout

--- messagesapp/urls.py
+++ messagesapp/urls.py
@@ -1,6 +1,24 @@
 from django.urls import path
-from . import views
+from .views import (
+    indexPageView,
+    registerPageView,
+    homePageView, 
+    logoutPageView # newPageView, loginPageView, messagePageView, replyPageView, deletePageView
+)
+
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", indexPageView, name="index"),
+    path("register/", registerPageView, name="register"),
+    path("home/", homePageView, name="home"),
+    path("logout/", logoutPageView, name="logout"),
 ]
+
+"""
+    ,
+    path("login/", loginPageView, name="login"),
+    path("new-message/", newPageView, name="new_message"),
+    path("message/", messagePageView, name="view_message"),
+    path("reply/", replyPageView, name="reply"),
+    path("delete-message/", deletePageView, name="delete_message"),
+"""
