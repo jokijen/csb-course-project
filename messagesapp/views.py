19f946bde7bb2a35f9777dd52f6eba315b6c1f48
Jenni Jokinen <153839491+jokijen@users.noreply.github.com>
2024-08-16 20:07:04 +0300

    Added registration, login and logout, but they are still under construction

--- messagesapp/views.py
+++ messagesapp/views.py
@@ -1,6 +1,62 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth import authenticate, login, logout
+from .models import Users, Conversations, Messages
+from .forms import RegistrationForm, LoginForm
+
+
+messages = [
+    {
+        'sender': 'Kimi',
+        'subject': 'Moi',
+        'content': 'Tässä koko viesti'
+    }, 
+     {
+        'sender': 'Mia',
+        'subject': 'Hei',
+        'content': 'Uusi viesti'
+    }
+]
 
 # Create your views here.
-def index(request):
-    return HttpResponse("Hello there! This is the index")
+
+def indexPageView(request):
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+
+        if form.is_valid():
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password"]
+            user = authenticate(request, username=username, password=password)
+            if user:
+                login(request, user)
+                return redirect("home")
+    else:
+        form = LoginForm()
+    return render(request, "index.html", {"form": form})
+
+
+def registerPageView(request):
+    if request.method == "POST":
+        form = RegistrationForm(request.POST)
+
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("home")
+    else:
+        form = RegistrationForm()
+
+    return render(request, "register.html", {"form": form})
+
+
+def logoutPageView(request):
+    logout(request)
+    return redirect("index")
+
+
+def homePageView(request):
+    context = {
+        'messages': messages
+        } #{"messages": Message.objects.all()}
+    return render(request, "home.html", context)
