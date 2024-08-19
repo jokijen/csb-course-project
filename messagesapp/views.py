9d858b4bae7aa2d6d69a7db333c4eddb9e003b72
Jenni Jokinen <153839491+jokijen@users.noreply.github.com>
2024-08-19 18:31:53 +0300

    Changed models to use django's built-in User. Initial idea was to have a design flaw of not using it, but this complicated things unnecessarily so I decided to revert to using it

--- messagesapp/views.py
+++ messagesapp/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login, logout
-from .models import Users, Conversations, Messages
+from django.contrib.auth.models import User
+from .models import Conversation, Message
 from .forms import RegistrationForm, LoginForm
 
 
