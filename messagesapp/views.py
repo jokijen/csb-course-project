ef6d81b305f5cc739ca917d125c0531705ed3987
Jenni Jokinen <153839491+jokijen@users.noreply.github.com>
2024-08-19 23:39:03 +0300

    Added error messages in case wrong username/password

--- messagesapp/views.py
+++ messagesapp/views.py
@@ -32,6 +32,8 @@ def indexPageView(request):
             if user:
                 login(request, user)
                 return redirect("home")
+            else:
+                form.add_error(None, "Invalid username or password. Please try again.")
     else:
         form = LoginForm()
     return render(request, "index.html", {"form": form})
